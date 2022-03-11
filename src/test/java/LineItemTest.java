4065164e7caf99751777a9215f7370907fc7e9a9
Rick DeBay <rick.debay@gmail.com>
2022-03-10 23:35:58 -0500

    Replace rushed test case with parameterized test case

--- src/test/java/LineItemTest.java
+++ src/test/java/LineItemTest.java
@@ -1,27 +1,35 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LineItemTest {
-    LineItem lineDove, lineAxe;
-    Product productDove, productAxe;
-
-    @BeforeEach
-    void setUp() {
-        productDove = new Product("Dove Soap");
-        lineDove = new LineItem(productDove);
-        lineDove.setQuantity((short)8);
-
-        productAxe = new Product("Axe Deo");
-        lineAxe = new LineItem(productAxe);
-        lineAxe.setQuantity((short)8);
+public class LineItemTest {
+    private static Stream<Arguments> testEqualsFixture() {
+        return Stream.of(
+                Arguments.of(new Product("Dove Soap"), (short)3, new Product("Axe Deo"), (short)3, false),
+                Arguments.of(new Product("Dove Soap"), (short)5, new Product("Dove Soap"), (short)99, true),
+                Arguments.of(new Product("Axe Deo"), (short)77, new Product("Dove Soap"), (short)77, false),
+                Arguments.of(new Product("bar"), (short)5, new Product("baz"), (short)4, false));
     }
 
-    @Test
-    void testEquals() {
-        assertEquals(lineDove, lineDove);
-        assertNotEquals(lineAxe, lineDove);
-        assertNotEquals(lineDove, productDove);
+    @ParameterizedTest
+    @MethodSource("testEqualsFixture")
+    public void testEquals(Product product1, short qtyLine1, Product product2, short qtyLine2, boolean success)
+    {
+        LineItem line1 = new LineItem(product1);
+        line1.setQuantity(qtyLine1);
+        LineItem line2 = new LineItem(product2);
+        line1.setQuantity(qtyLine2);
+        if (success)
+        {
+            assertEquals(line1, line2);
+        }
+        else
+        {
+            assertNotEquals(line1, line2);
+        }
     }
 }
