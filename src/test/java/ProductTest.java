279bf836819e96d585e77c11c7faddc7fdfcadc4
Rick DeBay <rick.debay@gmail.com>
2022-03-10 23:10:00 -0500

    Replace rushed test case with parameterized test case

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,34 +1,59 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ProductTest {
-    Product productDove, productAxe, productRoundUp, productRoundDown;
-    Object notaProduct;
+public class ProductTest {
 
-    @BeforeEach
-    void setUp() {
-        productDove = new Product("Dove Soap");
-        productDove.setCost(3999);
-        productAxe = new Product("Axe Deo");
-        productRoundUp = new Product("Round Up");
-        productRoundDown = new Product("Round Down");
+    private static Stream<Arguments> testScaledCostFixture() {
+        return Stream.of(
+                Arguments.of("good", 1234, "12.34", true),
+                Arguments.of("bad", 65478, "65.478", false),
+                Arguments.of("ugly", 123, "123.00", false));
     }
 
-    @Test
-    void testScaledCost()
+    @ParameterizedTest
+    @MethodSource("testScaledCostFixture")
+    public void testScaledCost(String productName, int productCost, String scaledCost, boolean success)
     {
-        BigDecimal doveCostAnswer = new BigDecimal("39.99");
-        assertEquals(doveCostAnswer, productDove.getScaledCost());
+        Product testProduct = new Product(productName);
+        testProduct.setCost(productCost);
+        BigDecimal actual = testProduct.getScaledCost();
+        BigDecimal scaledCostExpected = new BigDecimal(scaledCost);
+        if (success)
+        {
+            assertEquals(scaledCostExpected, actual);
+        }
+        else
+        {
+            assertNotEquals(scaledCostExpected, actual);
+        }
     }
 
-    @Test
-    void testEquals() {
-        assertEquals(productDove, productDove);
-        assertNotEquals(productAxe, productDove);
-        assertNotEquals(productDove, notaProduct);
+    private static Stream<Arguments> testEqualsFixture() {
+        return Stream.of(
+                Arguments.of(new Product("Dove Soap"), new Product("Axe Deo"), false),
+                Arguments.of(new Product("Dove Soap"), new Product("Dove Soap"), true),
+                Arguments.of(new Product("Axe Deo"), new Object(), false),
+                Arguments.of(new Product("blargh"), new Product(""), false),
+                Arguments.of(new Product(""), new Product("blargh"), false));
+    }
+
+    @ParameterizedTest
+    @MethodSource("testEqualsFixture")
+    public void testEquals(Object product1, Object product2, boolean success)
+    {
+        if (success)
+        {
+            assertEquals(product1, product2);
+        }
+        else
+        {
+            assertNotEquals(product1, product2);
+        }
     }
 }
