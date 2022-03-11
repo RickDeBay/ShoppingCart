6a31f79d9fad5d0cbef07263568bfd757ba3c5a8
Rick DeBay <rick.debay@gmail.com>
2022-03-11 18:23:22 -0500

    Finish unit tests

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -26,57 +26,142 @@ class CartTest {
         lineFoo = new LineItem(productFoo);
 
         cart = new Cart();
-        cart.setSalesTax(125);
     }
 
     @Test
-    void getTotalCost() {
+    void testGetTotalSalesTax_TaxRoundedUp() {
+        cart.setSalesTax(125);
+        cart.addItem(productDove, (short)22);
+        cart.addItem(productAxe, (short)11);
+
+        // 39.99 * 22 + 99.99 * 11 = 1979.67
+        // 1979.67 * 0.125 = 247.45875
+        BigDecimal expected = new BigDecimal("247.46");
+        BigDecimal actual = cart.getTotalSalesTax();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetTotalCost_WithTaxRoundedUp() {
+        cart.setSalesTax(125);
         cart.addItem(productDove, (short)22);
         cart.addItem(productAxe, (short)11);
 
         // 39.99 * 22 + 99.99 * 11 = 1979.67
         // 1979.67 * 1.125 = 2227.12875
-        BigDecimal answer = new BigDecimal("2227.13");
-        BigDecimal total = cart.getTotalCost();
-        assertEquals(total, answer);
+        BigDecimal expected = new BigDecimal("2227.13");
+        BigDecimal actual = cart.getTotalCost();
+        assertEquals(expected, actual);
     }
 
     @Test
-    void addLineItem() {
+    void testGetTotalSalesTax_TaxRoundedDown() {
+        cart.setSalesTax(125);
+        cart.addItem(productDove, (short)4);
+        cart.addItem(productAxe, (short)1);
+
+        // 39.99 * 4 + 99.99 * 1 = 259.95
+        // 259.95 * 0.125 = 32.49375
+        BigDecimal expected = new BigDecimal("32.49");
+        BigDecimal actual = cart.getTotalSalesTax();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetTotalCost_WithTaxRoundedDown() {
+        cart.setSalesTax(125);
+        cart.addItem(productDove, (short)4);
+        cart.addItem(productAxe, (short)1);
+
+        // 39.99 * 4 + 99.99 * 1 = 259.95
+        // 259.95 * 1.125 = 292.44375
+        BigDecimal expected = new BigDecimal("292.44");
+        BigDecimal actual = cart.getTotalCost();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetTotalSalesTax_ZeroTax() {
+        cart.setSalesTax(0);
+        cart.addItem(productDove, (short)4);
+        cart.addItem(productAxe, (short)1);
+
+        BigDecimal expected = new BigDecimal("0.00");
+        BigDecimal actual = cart.getTotalSalesTax();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetTotalSalesTax_ProveScalingTest() {
+        cart.setSalesTax(0);
+        cart.addItem(productDove, (short)4);
+
+        // sanity test scaling tests
+        BigDecimal expected = new BigDecimal("0");
+        BigDecimal actual = cart.getTotalSalesTax();
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    void testGetTotalCost_WithoutTax() {
+        cart.setSalesTax(0);
+        cart.addItem(productDove, (short)7);
+        cart.addItem(productAxe, (short)5);
+
+        // 39.99 * 7 + 99.99 * 5 = 1979.67
+        // 1979.67 * 1.000 = 2227.12875
+        BigDecimal expected = new BigDecimal("779.88");
+        BigDecimal actual = cart.getTotalCost();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAddLineItem_AddTwoLinesTwoProducts_GetTwoLinesTwoProducts() {
         cart.addItem(productDove, (short)22);
         cart.addItem(productAxe, (short)11);
 
         Collection<LineItem> lines = cart.getLineItems();
-        assertEquals(lines.size(), 2);
+        assertEquals(2, lines.size());
 
         for (LineItem line : lines)
         {
             Product product = line.getProduct();
             if (product.equals(productDove))
             {
-                assertEquals(line.getQuantity(), 22);
+                assertEquals(22, line.getQuantity());
             }
             else if (product.equals(productAxe))
             {
-                assertEquals(line.getQuantity(), 11);
+                assertEquals(11, line.getQuantity());
             }
             else
             {
+                // unknown product in cart
                 fail(product.getName());
             }
         }
     }
 
     @Test
-    void getTotalSalesTax()
-    {
-        cart.addItem(productDove, (short)22);
-        cart.addItem(productAxe, (short)11);
+    void testAddLineItem_AddProductTwice_GetOneLinesSummedProducts() {
+        cart.addItem(productDove, (short)3);
+        cart.addItem(productDove, (short)7);
 
-        // 39.99 * 22 + 99.99 * 11 = 1979.67
-        // 1979.67 * 0.125 = 247.45875
-        BigDecimal answer = new BigDecimal("247.46");
-        BigDecimal total = cart.getTotalSalesTax();
-        assertEquals(total, answer);
+        Collection<LineItem> lines = cart.getLineItems();
+        assertEquals(1, lines.size());
+
+        for (LineItem line : lines)
+        {
+            Product product = line.getProduct();
+            if (product.equals(productDove))
+            {
+                assertEquals(10, line.getQuantity());
+            }
+            else
+            {
+                // unknown product in cart
+                fail(product.getName());
+            }
+        }
     }
 }
